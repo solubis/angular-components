0c8dfde208571e863c867e8d166ef03480068358
yoorek <yoorek@gmail.com>
2016-01-10 13:51:32 +0100

    fix: mutliple

---
+++ dist/core.d.ts
@@ -0,0 +1,5 @@
+export * from './core/ConfigService';
+export * from './core/HttpInterceptor';
+export * from './core/RestService';
+export * from './core/SettingsService';
+export * from './core/UtilsService';
---
+++ dist/decorators.d.ts
@@ -0,0 +1,28 @@
+export interface IBasicDecoratorOptions {
+    name?: string;
+}
+export interface IComponentDecoratorOptions {
+    templateUrl?: string;
+    template?: string;
+    selector?: string;
+    name?: string;
+    dependencies?: string[];
+}
+export interface IConstructorTarget extends Function {
+    name: string;
+    prototype: any;
+}
+export interface IPrototypeTarget extends Object {
+    name: string;
+}
+declare function Service(name?: string): ClassDecorator;
+declare function Provider(name?: string): ClassDecorator;
+declare function Filter(name: string): PropertyDecorator;
+declare function Value(name?: string): PropertyDecorator;
+declare function Directive(name: string): PropertyDecorator;
+declare function ActionHandler(name: string): PropertyDecorator;
+declare function StoreListener(): (target: Object, propertyKey: string | symbol) => void;
+declare function Component(options: IComponentDecoratorOptions): ClassDecorator;
+declare function Inject(name?: string): any;
+declare function bootstrap(component: Function): void;
+export { Component, Inject, Service, Provider, Filter, Directive, Value, ActionHandler, StoreListener, bootstrap };
